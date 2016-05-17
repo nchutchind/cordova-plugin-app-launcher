721932b877ee8b7001e5b0e31873fb80eb8bef1d
Alexander Lyashenko <mrmasterix@gmail.com>
2016-05-17 17:34:39 +0300

    Update Launcher.m
    fix

--- src/ios/Launcher.m
+++ src/ios/Launcher.m
@@ -11,14 +11,14 @@
 		NSString *uri = [options objectForKey:@"uri"];
 		if ([[UIApplication sharedApplication] canOpenURL: [NSURL URLWithString:uri]]) {
 			 pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-			[self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
+			[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 		} else {
 			pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"No app installed that can handle that uri."];
-			[self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
+			[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 		}
 	} else {
 		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Missing option: 'uri' required."];
-		[self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
+		[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 	}
 }
 
@@ -32,14 +32,14 @@
 			NSURL *launchURL = [NSURL URLWithString:uri];
 			[[UIApplication sharedApplication] openURL: launchURL];
 			pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-			[self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
+			[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 		} else {
 			pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"No app installed that can handle that uri."];
-			[self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
+			[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 		}
 	} else {
 		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Missing option: 'uri' required."];
-		[self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
+		[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 	}
 }
 

46e11f303fc583caf6f33744ed85ca187e4ddbda
Alexander Lyashenko <mrmasterix@gmail.com>
2017-04-03 16:32:25 +0300

    ios writeJavascript depricated (#8)
    Updating ios launch commands.
    
    remove writeJavascript becouse it is depricated and replace them to [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
    
    * Update Launcher.m
    
    fix

--- src/ios/Launcher.m
+++ src/ios/Launcher.m
@@ -11,14 +11,14 @@
 		NSString *uri = [options objectForKey:@"uri"];
 		if ([[UIApplication sharedApplication] canOpenURL: [NSURL URLWithString:uri]]) {
 			 pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-			[self writeJavascript:[pluginResult toSuccessCallbackString:command.callbackId]];
+			[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 		} else {
 			pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"No app installed that can handle that uri."];
-			[self writeJavascript:[pluginResult toErrorCallbackString:command.callbackId]];
+			[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 		}
 	} else {
 		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Missing option: 'uri' required."];
-		[self writeJavascript:[pluginResult toErrorCallbackString:command.callbackId]];
+		[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 	}
 }
 
@@ -32,14 +32,14 @@
 			NSURL *launchURL = [NSURL URLWithString:uri];
 			[[UIApplication sharedApplication] openURL: launchURL];
 			pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-			[self writeJavascript:[pluginResult toSuccessCallbackString:command.callbackId]];
+			[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 		} else {
 			pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"No app installed that can handle that uri."];
-			[self writeJavascript:[pluginResult toErrorCallbackString:command.callbackId]];
+			[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 		}
 	} else {
 		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Missing option: 'uri' required."];
-		[self writeJavascript:[pluginResult toErrorCallbackString:command.callbackId]];
+		[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 	}
 }
 
